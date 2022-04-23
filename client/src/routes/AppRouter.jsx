224a85d1492227a854f090ea17da78e0a9211867
Sanketak08 <sakulkarni0801@gmail.com>
2022-04-24 00:35:55 +0530

    UI for few screens done

--- client/src/routes/AppRouter.jsx
+++ client/src/routes/AppRouter.jsx
@@ -1,57 +1,75 @@
-import { Grid, Link as CLink, Text, VStack, Box, Code, Button } from '@chakra-ui/react';
-import React from 'react'
-import { useDispatch } from 'react-redux';
+import {
+  Grid,
+  Link as CLink,
+  Text,
+  VStack,
+  Box,
+  Code,
+  Button,
+} from "@chakra-ui/react";
+import React from "react";
+import { useDispatch } from "react-redux";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { ColorModeSwitcher } from '../components/ColorModeSwitcher';
-import { Logo } from '../components/Logo';
-import CreateBlog from '../pages/CreateBlog';
-import Private from '../pages/Private';
-import { setAuth } from '../redux/auth/slice';
-import ProtectedRoute from './ProtectedRoute';
-
+import { ColorModeSwitcher } from "../components/ColorModeSwitcher";
+import { Logo } from "../components/Logo";
+import Navbar from "../components/Navbar";
+import CreateBlog from "../pages/CreateBlog";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Newsletter from "../pages/Newsletter";
+import Private from "../pages/Private";
+import { setAuth } from "../redux/auth/slice";
+import ProtectedRoute from "./ProtectedRoute";
 
 const AppRouter = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Sample />} />
-        <Route path="create-blog" element={<CreateBlog />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/newsletter" element={<Newsletter />} />
+        <Route path="create-blog" element={<ProtectedRoute />}>
+          <Route path="" element={<CreateBlog />} />
+        </Route>
         <Route path="private" element={<ProtectedRoute />}>
           <Route path="" element={<Private />} />
         </Route>
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
+// const Sample = () => {
+//   const dispatch = useDispatch();
+//   const login = () => {
+//     dispatch(setAuth({ authState: "LOGGEDIN" }));
+//   };
+//   return (
+//     <Box textAlign="center" fontSize="xl">
+//       <Grid minH="100vh" p={3}>
+//         <ColorModeSwitcher justifySelf="flex-end" />
+//         <VStack spacing={8}>
+//           <Logo h="40vmin" pointerEvents="none" />
+//           <Text>
+//             Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
+//           </Text>
+//           <CLink
+//             color="teal.500"
+//             href="https://chakra-ui.com"
+//             fontSize="2xl"
+//             target="_blank"
+//             rel="noopener noreferrer">
+//             Learn Chakra
+//           </CLink>
+//           <Button onClick={login}>Login</Button>
+//           <Link to={"private"}>
+//             <CLink>Private page</CLink>
+//           </Link>
+//         </VStack>
+//       </Grid>
+//     </Box>
+//   );
+// };
 
-const Sample = () => {
-  const dispatch = useDispatch();
-  const login = () => {
-    dispatch(setAuth({ authState: "LOGGEDIN" }))
-  }
-  return <Box textAlign="center" fontSize="xl">
-    <Grid minH="100vh" p={3}>
-      <ColorModeSwitcher justifySelf="flex-end" />
-      <VStack spacing={8}>
-        <Logo h="40vmin" pointerEvents="none" />
-        <Text>
-          Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-        </Text>
-        <CLink
-          color="teal.500"
-          href="https://chakra-ui.com"
-          fontSize="2xl"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Chakra
-        </CLink>
-        <Button onClick={login}>Login</Button>
-        <Link to={"private"}><CLink>Private page</CLink></Link>
-      </VStack>
-    </Grid>
-  </Box>
-}
-
-export default AppRouter
+export default AppRouter;
--- server/models/Blog.js
+++ server/models/Blog.js
@@ -2,38 +2,42 @@ const mongoose = require("mongoose");
 const { MODELS } = require("../config/constants");
 
 const Blog = mongoose.model(
-    MODELS.BLOG,
-    new mongoose.Schema({
-        title: {
-            type: String,
-            required: true,
-        },
-        poster: {
-            type: String,
-            required: true,
-        },
-        description: {
-            type: String,
-            required: true,
-        },
-        content: {
-            type: String,
-            required: true,
-        },
-        isDraft: {
-            type: Boolean,
-            default: true,
-        },
-        publishedAt: {
-            type: Date,
-            default: Date.now(),
-        },
-        publishedBy: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: MODELS.USER,
-            required: true,
-        },
-    })
+  MODELS.BLOG,
+  new mongoose.Schema({
+    title: {
+      type: String,
+      required: true,
+    },
+    poster: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    isDraft: {
+      type: Boolean,
+      default: true,
+    },
+    publishedAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    publishedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: MODELS.USER,
+      required: true,
+    },
+    isTop: {
+      type: Boolean,
+      default: false,
+    },
+  })
 );
 
 module.exports = Blog;
